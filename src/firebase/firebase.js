2cb5978cfdc1e2da3c62e6b3fe06e691b92fd0bc
huseyintosun <huseyin.tosun.8818@gmail.com>
2021-10-09 22:14:41 +0300

    last

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@
 // import { initializeApp } from "firebase/app";
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/firestore";
+// import "firebase/firestore";
 import "firebase/database";
 
 
@@ -12,15 +12,22 @@ import "firebase/database";
 
 
 const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyBJ6S4fn_-fIM8lDsQYosCXlmZ9TWdz2iw",
+  authDomain: "geosys-case.firebaseapp.com",
+  databaseURL: "https://geosys-case-default-rtdb.firebaseio.com",
+  projectId: "geosys-case",
+  storageBucket: "geosys-case.appspot.com",
+  messagingSenderId: "819375720305",
+  appId: "1:819375720305:web:42adce5cab651fcd8498d0",
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // databaseURL: process.env.REACT_APP_DATABASE_URL,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_APP_ID,
 };
-const prodConfig = {};
-const firebaseConfig = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
-firebase.initializeApp(firebaseConfig);
+// const prodConfig = {};
+// const firebaseConfig = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
+firebase.initializeApp(devConfig);
 export default firebase;
